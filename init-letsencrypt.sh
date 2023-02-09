ee297c9d4340e458f27040f1e77634a0eefc0463
Shawn Jackson <shawn@resgrid.com>
2023-02-09 08:12:39 -0800

    Minor path fix

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -12,7 +12,7 @@ fi
 
 domains=($NGINX_RESGRID_WEB_URL $NGINX_RESGRID_API_URL $NGINX_RESGRID_EVENTS_URL)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="./docker-data/certbot"
 email=$NGINX_LETSENCRYPT_EMAIL # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
