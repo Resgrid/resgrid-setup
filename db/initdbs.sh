7833d205fc5dedcc52744da65d074787194deb82
Deneir Uy <deneiruy@gmail.com>
2024-10-09 13:44:21 +0800

    Update db init scripts

--- db/initdbs.sh
+++ db/initdbs.sh
@@ -1,2 +1,2 @@
 sleep 90s
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Resgrid123!! -d master -i create-databases.sql
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P Resgrid123!! -d master -i create-databases.sql
