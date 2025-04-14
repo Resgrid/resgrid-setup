c0ad3ed2ca342ea740f22852578929c6cc072772
Shawn Jackson <shawn@resgrid.com>
2025-04-14 08:19:46 -0700

    pgsql entrypoint change.

--- db/custom-entrypoint.sh
+++ db/custom-entrypoint.sh
@@ -5,11 +5,13 @@ set -e
 docker-entrypoint.sh postgres &
 
 # Wait for PostgreSQL to be ready
-until pg_isready -U $POSTGRES_USER -d $POSTGRES_DB; do
+echo "Waiting for PostgreSQL to be ready..."
+until pg_isready -h localhost -p 5432 -U $POSTGRES_USER; do
   echo "Waiting for PostgreSQL to be ready..."
   sleep 1
 done
 
+echo "PostgreSQL is ready - creating databases..."
 # Run our database creation script
 /docker-entrypoint-initdb.d/create-databases.sh
 
