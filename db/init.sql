3535c0edbce4ae130487e14cd4b297232587eea9
Shawn Jackson <shawn@resgrid.com>
2025-04-13 18:27:59 -0700

    Trying to fix db script

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,9 @@
 
-SELECT ‘CREATE DATABASE resgrid WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'resgrid')
-SELECT ‘CREATE DATABASE resgridoidc WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'resgridoidc')
-SELECT ‘CREATE DATABASE resgridworkers WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'resgridworkers')
+SELECT 'CREATE DATABASE resgrid'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'resgrid')
+
+SELECT 'CREATE DATABASE resgridoidc'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'resgridoidc')
+
+SELECT 'CREATE DATABASE resgridworkers'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'resgridworkers')
