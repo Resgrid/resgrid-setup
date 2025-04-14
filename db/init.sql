8c0e3a6aeb5ba31c5e9c327afecad660d230a23c
Shawn Jackson <shawn@resgrid.com>
2025-04-13 21:09:45 -0700

    More work on script

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,9 @@
 
 SELECT 'CREATE DATABASE resgrid'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'resgrid')
+WHERE NOT EXISTS (SELECT * FROM pg_database WHERE datname = 'resgrid')
 
 SELECT 'CREATE DATABASE resgridoidc'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'resgridoidc')
+WHERE NOT EXISTS (SELECT * FROM pg_database WHERE datname = 'resgridoidc')
 
 SELECT 'CREATE DATABASE resgridworkers'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'resgridworkers')
+WHERE NOT EXISTS (SELECT * FROM pg_database WHERE datname = 'resgridworkers')
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-sysctl -w vm.max_map_count=262144
+sudo sysctl -w vm.max_map_count=262144
 mkdir -p docker-data/sql/
-docker-compose --project-name resgird up
+sudo docker compose up -d
