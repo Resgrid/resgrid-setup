5834c2ff8e750ed3504e42f62261e80af3bfd2c6
Shawn Jackson <shawn@resgrid.com>
2025-04-14 18:08:53 -0700

    Adding missing extension

--- db/create-databases.sh
+++ db/create-databases.sh
@@ -5,6 +5,7 @@ create_db() {
     local dbname=$1
     echo "Creating PostgreSQL database $dbname"
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -tc "SELECT 1 FROM pg_database WHERE datname = '$dbname'" | grep -q 1 || psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE $dbname"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d $dbname -c "CREATE EXTENSION IF NOT EXISTS \"citext\";"
 }
 
 # Create the databases
